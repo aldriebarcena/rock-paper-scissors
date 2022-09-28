4a14bdf50d0254ed7bc7436d2e35ab8d8c3102f6
Aldrie Barcena <aldrievbarcena@gmail.com>
2022-09-27 22:30:15 -0700

    created comments to minimize switching to odinproject directions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hi')
+// game played through console
+// console.log to test return statments!!!!!
+
+// game played against computer, function getComputerChoice
+// returns rock, paper, scissors RANDOMLY
+// use console to test
+
+
+// computerSelection
+
+// playerSelection
+// use prompt()
+
+
+
+// function that plays round of RPS (rock paper scissors)
+// takes 2 parameter: playerSelection and computerSelection
+// returns win/lose/tie
+// player selection case-INsensitive
+
+// function called game
+// playRound function INSIDE of game() to play 5 round game
+// keeps score, reports overall winner/loser with console.log
+// use loops
+
+//rework functions if needed
+// "helper" functions
