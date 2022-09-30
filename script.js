e536b97478ee0652648540300ad386f0ffefd7d2
Aldrie Barcena <aldrievbarcena@gmail.com>
2022-09-29 21:43:31 -0700

    fixed code so that it detects selections as string

--- script.js
+++ script.js
@@ -5,49 +5,29 @@
 // returns rock, paper, scissors RANDOMLY
 // use console to test
 
-// computerSelection
 function getComputerChoice() {
     const choiceList = [
-        "rock",
-        "paper",
-        "scissors"
+        'rock',
+        'paper',
+        'scissors'
     ]
     const choiceRandom = choiceList[Math.floor(Math.random()*choiceList.length)];
-    // random choice syntax = arrayVariableName[Math.floor(Math.random()*arrayVariableName.length)]
     return choiceRandom
 }
-console.log(getComputerChoice())
+const computerChoice = getComputerChoice()
 
 
-// playerSelection
-// use prompt()
-    function playerSelection() {
-        const selection = prompt("Rock, Paper, or Scissors: ")
-        selection.toLowerCase()
-        return selection
-    }
-    console.log(playerSelection())
+function playerSelection() {
+    const selection = prompt("Rock, Paper, or Scissors: ")
+    selection.toLowerCase()
+    return selection
+}
+const playerChoice = playerSelection()
+console.log(computerChoice)
 
-// function that plays round of RPS (rock paper scissors)
-// takes 2 parameter: playerSelection and computerSelection
-// returns win/lose/tie
-// player selection case-INsensitive
- function playGame(){
-    if (getComputerChoice() === 'rock') {
-        if (playerSelection() === 'paper') {
-            console.log('You win!')
-        } else if (playerSelection() === 'scissors') {
-            console.log('You lose!') 
-        } else {
-            console.log('Tie!')
-        }
-    } else {
-        console.log('test')
-    }
 
- }
 
- console.log(playGame())
+
 
 // function called game
 // playRound function INSIDE of game() to play 5 round game
